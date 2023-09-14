7e054305a9d07e6dd99adde238a66ac79de9fb7a
nail111 <nailtagiyev1999@gmail.com>
2023-09-14 16:54:57 +0400

    add ResourceNotFoundException

---
+++ src/main/java/org/example/exception/ResourceNotFoundException.java
@@ -0,0 +1,20 @@
+package org.example.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+@Getter
+public class ResourceNotFoundException extends RuntimeException {
+    private String resourceName;
+    private String fieldName;
+    private String fieldValue;
+
+    public ResourceNotFoundException(String resourceName, String fieldName, String fieldValue) {
+        super(String.format("%s is not found with %s: '%s'", resourceName, fieldName, fieldValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+}
