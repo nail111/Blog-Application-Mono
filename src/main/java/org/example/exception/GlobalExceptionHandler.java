425def161d5b55bd5fa9b6fd0b665abf17fd21dc
nail111 <nailtagiyev1999@gmail.com>
2023-09-15 15:42:04 +0400

    add DataIntegrityViolationException handling

--- src/main/java/org/example/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/exception/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package org.example.exception;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -8,18 +8,36 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> handleValidationError(MethodArgumentNotValidException methodArgumentNotValidException) {
+    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException methodArgumentNotValidException) {
         List<String> error = methodArgumentNotValidException
                 .getFieldErrors()
                 .stream()
                 .map(FieldError::getDefaultMessage)
                 .toList();
 
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(error, BAD_REQUEST);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<?> handleDataIntegrityViolationException(DataIntegrityViolationException dataIntegrityViolationException) {
+        String errorMessage = dataIntegrityViolationException.getMessage();
+        String errorResponse = null;
+        if (errorMessage.contains("Duplicate entry")) {
+            Pattern pattern = Pattern.compile("Duplicate entry '([^']+)' for key '([^']+)'");
+            Matcher matcher = pattern.matcher(errorMessage);
+            if (matcher.find()) {
+                errorResponse = matcher.group();
+            }
+        }
+        return new ResponseEntity<>(errorResponse, BAD_REQUEST);
     }
 }
