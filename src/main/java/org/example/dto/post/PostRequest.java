0f9d95f696485aaf45e257c07c59b4710e0c12a9
Nail <nailtagiyev1999@gmail.com>
2023-10-08 19:24:47 +0400

    add @Schema annotation, finish Swagger

--- src/main/java/org/example/dto/post/PostRequest.java
+++ src/main/java/org/example/dto/post/PostRequest.java
@@ -1,5 +1,6 @@
 package org.example.dto.post;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -13,23 +14,30 @@ import org.hibernate.validator.constraints.Length;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Schema(
+        description = "PostRequest Model Information"
+)
 public class PostRequest {
 
     @NotNull(message = "title can not be null")
     @NotBlank(message = "title can not be empty")
     @Size(min = 3, max = 255, message = "title size is min 3 and max 255")
+    @Schema(description = "Blog Post title")
     private String title;
 
     @NotNull(message = "description can not be null")
     @NotBlank(message = "description can not be empty")
     @Size(min = 3, max = 255, message = "description size is min 3 and max 255")
+    @Schema(description = "Blog Post description")
     private String description;
 
     @NotNull(message = "content can not be null")
     @NotBlank(message = "content can not be empty")
     @Size(min = 3, max = 255, message = "content size is min 3 and max 255")
+    @Schema(description = "Blog Post content")
     private String content;
 
     @NotNull(message = "categoryId can not be null")
+    @Schema(description = "Blog Post categoryId")
     private Long categoryId;
 }
